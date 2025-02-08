d6232b369cad38a404f3d2eaead0dce5a70a8280
Chew Poshi <chewposhi@gmail.com>
2025-02-08 23:12:58 +0800

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -13,7 +13,7 @@ export default function Hero() {
           Chew Poshi
         </h2>
         <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-          Developer, Scuba Diver, Free Diver, Spear Fisherman
+          Developer | Scuba Diver | Free Diver | Spear Fisherman
         </h3>
         <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
           <GiScubaTanks />
@@ -23,7 +23,7 @@ export default function Hero() {
         <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
           Into all kinds of water sports, especially diving. Join me down below on my diving trips!
         </p>
-        <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+        <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-10 md:h-96 md:w-96">
           <Image src={profile} layout="fill" objectFit="cover" />
         </div>
       </div>
