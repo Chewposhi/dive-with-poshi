31299a6c2e260ad2b3662e01e77ae29b4fc3f499
Chew Poshi <chewposhi@gmail.com>
2025-02-09 19:37:27 +0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // Utility to handle API calls
-console.log("api", process.env.NEXT_PUBLIC_API_UR)
+console.log("api", process.env.NEXT_PUBLIC_API_URL)
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_UR,
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     timeout: 10000, // Set a timeout for requests
 });
