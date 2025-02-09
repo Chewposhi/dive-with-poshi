47939f569364b43d76511187cb349d66b0cac223
Chew Poshi <chewposhi@gmail.com>
2025-02-09 20:02:05 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   target: 'serverless', // Required for Netlify to handle SSR and API routes
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
